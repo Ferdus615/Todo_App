e66521e8e30121bf8b4ba89ef0df151b0c75f384
ferdus615 <ferdus615@gmail.com>
2025-10-06 21:21:21 +0600

    home old code

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,9 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
-  
+  @Post('/')
+  createTodo(@Body())
+
 }
