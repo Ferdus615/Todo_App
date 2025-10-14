7e52a6b59318ba7b47d8cdcc990c82b277cb3ab2
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-10-14 10:31:13 +0600

    class-validator added

--- src/todos/dto/todoResponse.dto.ts
+++ src/todos/dto/todoResponse.dto.ts
@@ -1,8 +1,21 @@
-export interface TodoResponseDto {
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+
+export class TodoResponseDto {
+  @IsNumber()
   id: number;
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsBoolean()
   isCompleted: boolean;
+
+  @IsBoolean()
   isArchived: boolean;
+
+  @IsBoolean()
   isDeleted: boolean;
 }
