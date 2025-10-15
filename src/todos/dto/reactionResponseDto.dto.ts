3b05bc91f5562baebf749c4afd1bdfb4d461bc45
ferdus615 <ferdus615@gmail.com>
2025-10-15 10:59:55 +0600

    added reactionResponseDto

---
+++ src/todos/dto/reactionResponseDto.dto.ts
@@ -0,0 +1,28 @@
+import { IsDateString, IsEnum, IsNumber, IsOptional } from 'class-validator';
+
+export enum ReactionType {
+  LIKE = 'like',
+  LOVE = 'love',
+  HAHA = 'haha',
+  WOW = 'wow',
+  SAD = 'sad',
+  ANGRY = 'angry',
+}
+
+export class ReactionResponseDto {
+  @IsNumber()
+  id: number;
+
+  @IsNumber()
+  todo_id: number;
+
+  @IsEnum(ReactionType)
+  reaction_type: ReactionType;
+
+  @IsOptional()
+  @IsNumber()
+  user_id?: number;
+
+  @IsDateString()
+  created_at: string;
+}
