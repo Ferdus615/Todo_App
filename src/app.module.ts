149c5a4804eca70a18408dd7669f8d4add4de281
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-10-23 11:47:09 +0600

    trying to add user registration, login & logout

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { PostReactionModule } from './post_reaction/post_reaction.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule, PostReactionModule],
+  imports: [TodosModule, PostReactionModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
