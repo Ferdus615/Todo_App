02e6b0663e81b3b7211df401aec3878cd48432bc
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-10-30 10:17:36 +0600

    home old code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  import: [
+    UserModule, 
+    JwtModule.register({
+      global
+    })
+  ]
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
