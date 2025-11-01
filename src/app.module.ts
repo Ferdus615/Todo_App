ad7faeca474d6cdb09e620587d1342753acbde4a
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-11-01 21:10:16 +0600

    old home code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { PostReactionModule } from './post_reaction/post_reaction.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule, PostReactionModule, UserModule, AuthModule],
+  imports: [TodosModule, PostReactionModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
