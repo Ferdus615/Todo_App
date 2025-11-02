c668655f383f684f06ee04a8bb9126d6eb6ebb63
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-11-03 02:54:33 +0600

    auth service is done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import {
   ConflictException,
   Injectable,
-  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { access } from 'fs';
 import { CreateUserDto } from 'src/users/dto/createUserDto.dto';
 import { ResponseUserDto } from 'src/users/dto/responseUserDto.dto';
 import { UsersService } from 'src/users/users.service';
@@ -30,6 +32,34 @@ export class AuthService {
       message: 'User register successfully',
       access_token: await this.jwtService.signAsync(payload),
       user: newUser,
+    };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.userService.getUserByEmail(email);
+    if (!user) throw new UnauthorizedException('Invalid email or password!');
+
+    const matchPass = await bcrypt.compare(password, user.password);
+    if (!matchPass)
+      throw new UnauthorizedException('Invalid email or password!');
+
+    const payload = {
+      sub: user.id,
+      name: `${user.first_name} ${user.last_name}`,
+      email: user.email,
+    };
+
+    return {
+      messsage: 'Login successful',
+      access_token: await this.jwtService.signAsync(payload),
+      user,
+    };
+  }
+
+  async logout() {
+    return {
+      message:
+        'Logout successful. Token invalidation not persisted (stateless JWT).',
     };
   }
 }
