4231d6c98984fe3018f276570e49ebb1e8784cd4
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-11-03 02:33:07 +0600

    stated the auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/createUserDto.dto';
+import { ResponseUserDto } from 'src/users/dto/responseUserDto.dto';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
@@ -9,5 +15,21 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  
+  async register(dto: CreateUserDto): Promise<ResponseUserDto> {
+    const existingUser = await this.userService.getUserByEmail(dto.email);
+    if (!existingUser) throw new ConflictException('Email already exists!');
+
+    const newUser = await this.userService.createUser(dto);
+    const payload = {
+      sub: newUser.id,
+      name: `${newUser.first_name} ${newUser.last_name}`,
+      email: newUser.email,
+    };
+
+    return {
+      message: 'User register successfully',
+      access_token: await this.jwtService.signAsync(payload),
+      user: newUser,
+    };
+  }
 }
